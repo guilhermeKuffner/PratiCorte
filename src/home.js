e8989d931283a3ca47bca3af471dc34da481b96c
Maick Duarte <maickduarte.2@hotmail.com>
2025-01-24 22:40:11 -0300

    montando html da homepage

--- src/home.js
+++ src/home.js
@@ -13,8 +13,30 @@ class Home extends React.Component {
     render() {
         return (
             <>
-                <h1>{this.state.projeto}</h1>
-                <h1>{this.state.data}</h1>
+                <div>
+                    <div>
+                        <img src='' alt='' />
+                        <span>Gestão rapida e fácil da sua barbearia.</span>
+                    </div>
+                    <form>
+                        <div>
+                            <label htmlFor='email'>E-mail</label>
+                            <input type='text' name='email' id='email' placeholder='exemplo@gmail.com'
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor='password'>Senha</label>
+                            <input type='password' name='password' id='password' placeholder='***********'
+                            />
+                        </div>
+                    </form>
+                    <a href=''>Esqueceu sua senha?</a>
+                    <button>Fazer Login</button>
+                    <div>
+                        <span>Ainda não é cliente?</span>
+                        <a href=''>Crie sua conta!</a>
+                    </div>
+                </div>
             </>
         )
     }
