9871c04bd70ee461415e0a5105f0112475dce36d
Maick Duarte <maickduarte.2@hotmail.com>
2025-02-04 23:04:47 -0300

    separando e redirecionando tela de login e tela de cadastro

--- src/home.js
+++ src/home.js
@@ -1,33 +1,65 @@
 import React from "react";
+
+const initialState = {
+    screenType: 'login',
+}
+
 class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+    }
+
+    showRegisterPage = () => {
+        this.setState({ screenType: "register" })
+    }
+
+    showLoginPage = () => {
+        this.setState({ screenType: "login" })
+    }
+
     render() {
         return (
             <>
-                <div className="container d-flex flex-column justify-content-center align-items-center">
-                    <div className="card p-4 shadow-lg bg-white rounded">
-                        <div>
-                            <img src="" alt="" />
-                            <span>Gestão rapida e fácil da sua barbearia.</span>
-                        </div>
-                        <form>
-                            <div className="mb-3">
-                                <label className="form-label" htmlFor="email">E-mail</label>
-                                <input className="form-control" type="text" name="email" id="email" placeholder="exemplo@gmail.com"/>
-                            </div>
+                { 
+                    this.state.screenType === "login" &&
+                    <div className="container d-flex flex-column justify-content-center align-items-center">
+                        <div className="card p-4 shadow-lg bg-white rounded">
                             <div>
-                                <label className="form-label" htmlFor="password">Senha</label>
-                                <input className="form-control" type="password" name="password" id="password" placeholder="***********"
-                                />
+                                <img src="" alt="" />
+                                <span>Gestão rapida e fácil da sua barbearia.</span>
                             </div>
-                        </form>
-                        <a href="" className="d-block mb-3 text-decoration-none text-center">Esqueceu sua senha?</a>
-                        <button className="btn btn-primary mb-3">Fazer Login</button>
-                        <div className="text-center">
-                            <span className="d-block mb-2">Ainda não é cliente?</span>
-                            <a href="" className="btn btn-outline-secondary">Crie sua conta!</a>
+                            <form>
+                                <div className="mb-3">
+                                    <label className="form-label" htmlFor="email">E-mail</label>
+                                    <input className="form-control" type="text" name="email" id="email" placeholder="exemplo@gmail.com"/>
+                                </div>
+                                <div>
+                                    <label className="form-label" htmlFor="password">Senha</label>
+                                    <input className="form-control" type="password" name="password" id="password" placeholder="***********"
+                                    />
+                                </div>
+                            </form>
+                            <a href="" className="d-block mb-3 text-decoration-none text-center">Esqueceu sua senha?</a>
+                            <button className="btn btn-primary mb-3">Fazer Login</button>
+                            <div className="text-center">
+                                <span className="d-block mb-2">Ainda não é cliente?</span>
+                                <button href="" className="btn btn-outline-secondary" onClick={this.showRegisterPage}>Crie sua conta!</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                }
+                {
+                    this.state.screenType === "register" &&
+                    <div>
+                        tela de registro
+                        <div className="text-center">
+                            <span className="d-block mb-2">Já é cliente?</span>
+                            <button href="" className="btn btn-outline-secondary" onClick={this.showLoginPage}>Voltar para tela de login!</button>
+                        </div>
+                    </div>
+                }
             </>
         )
     }
