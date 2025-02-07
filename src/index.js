f2ce04cbbdda48a17c1cfce8717d2097b301f527
Maick Duarte <maickduarte.2@hotmail.com>
2025-02-06 22:53:41 -0300

    mudando hospedagem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { App } from './App';
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,8 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
