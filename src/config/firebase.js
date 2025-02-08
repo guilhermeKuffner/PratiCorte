238dcdf0426f931227feca00a42ef26ceb54b1bd
Maick Duarte <maickduarte.2@hotmail.com>
2025-02-07 22:55:38 -0300

    importando banco de dados firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAEfxO9C3lNjod2fG9Z1cQIUk6nNRDCic8",
@@ -12,4 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+const auth = getAuth(app)
+const db = getFirestore(app);
+
+export { auth, db };
