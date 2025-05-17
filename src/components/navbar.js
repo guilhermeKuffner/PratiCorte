c16647fec1fce9f3a0412305329194db9ccf9b99
Maick Duarte <maickduarte.2@hotmail.com>
2025-05-17 19:54:01 -0300

    ajuste NavLink

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,26 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export const NavBar = props => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top w-100">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">PratiCorte</a>
+                    <NavLink to="/home" className="navbar-brand">
+                        PratiCorte
+                    </NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <Link to="/home" className="nav-item">
-                                <button className="nav-link active" aria-current="page" href="">Home</button>
-                            </Link>
-                            <Link to="/link1"  className="nav-item">
-                                <button className="nav-link" href="">Link</button>
-                            </Link>
-                            <Link to="/link2" className="nav-item">
-                                <button className="nav-link" href="">Link</button>
-                            </Link>
+                            <NavLink to="/home" className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}>
+                                Home
+                            </NavLink>
+                            <NavLink to="/link1" className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}>
+                                Link
+                            </NavLink>
+                            <NavLink to="/link1" className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}>
+                                Link
+                            </NavLink>
                         </ul>
                     </div>
                 </div>
@@ -27,5 +29,5 @@ export const NavBar = props => {
                 {props.children}
             </div>
         </>
-    );
-};
+    )
+}
